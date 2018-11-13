9e58d02d0a39b6dcc186fbc5d85ff3932a40c0da
MizoTake <mizotake123@gmail.com>
2018-11-14 00:28:23 +0900

    delete namespace

--- Assets/Message.cs
+++ Assets/Message.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace sLowZoo
+public class Message : ScriptableObject
 {
-	public class Message : ScriptableObject
-	{
-		public Talk[] flow;
+	public Talk[] flow;
 
-		[System.Serializable]
-		public class Talk
-		{
-			public int id;
-			public string name;
-			public string message;
-		}
+	[System.Serializable]
+	public class Talk
+	{
+		public int id;
+		public string name;
+		public string message;
 	}
 }
