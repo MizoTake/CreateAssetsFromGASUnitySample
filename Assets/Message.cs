aee482821993ef10ee5bd6d2e57dcf8985cace4c
MizoTake <mizotake123@gmail.com>
2018-11-14 00:27:21 +0900

    first

---
+++ Assets/Message.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace sLowZoo
+{
+	public class Message : ScriptableObject
+	{
+		public Talk[] flow;
+
+		[System.Serializable]
+		public class Talk
+		{
+			public int id;
+			public string name;
+			public string message;
+		}
+	}
+}
